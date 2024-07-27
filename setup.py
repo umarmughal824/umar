0ebecffbc0c06da87d745683fa1284fb4c2bc8b0
Umar Asghar <mrumarasghar@gmail.com>
2024-07-27 19:58:59 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="umar",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple example package",
+    author="Umar Asghar",
+    author_email="mrumarasghar@gmail.com",
+    url="https://github.com/umarmughal824/umar",
+)
